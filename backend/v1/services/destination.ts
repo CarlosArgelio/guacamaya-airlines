d301987b2b2def6d447ef2117f2a66d21a152132
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-13 17:14:21 -0400

    feat: add service detination

---
+++ backend/v1/services/destination.ts
@@ -0,0 +1,28 @@
+export class DestinationService {
+  constructor(private readonly database: any = []) {}
+
+  findAll() {
+    return this.database
+  }
+  findOne(id: string) {
+    const airPort = this.database.find((item: any) => item.id === id)
+    return airPort
+  }
+  create(data: any) {
+    this.database.push(data)
+    return data
+  }
+  update(id: string, changes: any) {
+    const index = this.database.findIndex((item: any) => item.id === id)
+    this.database[index] = {
+      ...this.database[index],
+      ...changes,
+    }
+    return this.database[index]
+  }
+  remove(id: string) {
+    const index = this.database.findIndex((item: any) => item.id === id)
+    this.database.splice(index, 1)
+    return
+  }
+}
