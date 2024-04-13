fbb0d3396d40ff264e3c52d616949d266a1d01a3
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-13 16:40:42 -0400

    feat: add controller reservation

---
+++ backend/v1/controllers/reservation.ts
@@ -0,0 +1,30 @@
+import { uuid as v4 } from 'uuidv4'
+
+import { ReservationService } from '../services/reservations'
+import { ResendService } from '../services/email'
+
+export class ReservationController {
+  constructor(
+    private reservationService = new ReservationService(),
+    private emailService = new ResendService(),
+  ) {}
+
+  async create(data: any) {
+    // const { email } = data
+    const id = v4()
+
+    const newReservation = {
+      id,
+      ...data,
+    }
+
+    const reservation =
+      await this.reservationService.createReservation(newReservation)
+    // await this.emailService.sendEmail(
+    //   email,
+    //   'Hello World',
+    //   '<p>This is a testing</p>',
+    // )
+    return reservation
+  }
+}
