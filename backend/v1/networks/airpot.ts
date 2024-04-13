5e2de91273fe7f5cc4cd6e99f38c7236598500e5
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-13 17:07:26 -0400

    feat: add network airport

---
+++ backend/v1/networks/airpot.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response, Router } from 'express'
+import { AirPortController } from '../controllers/airport'
+
+const airpot = Router()
+
+const findAll = (req: Request, res: Response, nex: NextFunction) => {
+  const controller = new AirPortController()
+  const airports = controller.findAll()
+  res.json(airports)
+}
+const findOne = (req: Request, res: Response, nex: NextFunction) => {
+  const { id } = req.params
+
+  const controller = new AirPortController()
+  const airport = controller.findOne(id)
+  res.json(airport)
+}
+const create = (req: Request, res: Response, nex: NextFunction) => {
+  const data = req.body
+
+  const controller = new AirPortController()
+  const newAirPort = controller.create(data)
+  res.status(201).json(newAirPort)
+}
+const update = (req: Request, res: Response, nex: NextFunction) => {
+  const { id } = req.params
+  const changes = req.body
+
+  const controller = new AirPortController()
+  const updateAirport = controller.update(id, changes)
+  return updateAirport
+}
+const remove = (req: Request, res: Response, nex: NextFunction) => {
+  const { id } = req.params
+
+  const controller = new AirPortController()
+  controller.remove(id)
+
+  res.status(204).json()
+}
+
+airpot.get('/airpots', findAll)
+airpot.get('/airpots/:id', findOne)
+airpot.post('/airpots', create)
+airpot.put('/airpots/:id', update)
+airpot.delete('/airpots/:id', remove)
