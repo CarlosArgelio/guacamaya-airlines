a369bffa97fc7d55ca904460f95d320dda1f9f28
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-15 18:35:54 -0400

    feat: add middleware response to reservation

--- backend/v1/networks/reservation.ts
+++ backend/v1/networks/reservation.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import { ReservationController } from '../controllers'
+import { success } from './../middlewares'
 
 export const reserver = Router()
 
 const create = (req: Request, res: Response, next: NextFunction) => {
   const data = req.body
-
-  const controller = new ReservationController()
-  const newReserver = controller.create(data)
-  res.status(201).json(newReserver)
+  try {
+    const controller = new ReservationController()
+    const newReserver = controller.create(data)
+    success(req, res, newReserver, 201)
+  } catch (error) {
+    next(error)
+  }
 }
 
 reserver.post('/', create)
