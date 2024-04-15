e4ec5b94d1be98aa23a0791c5fcee99d0f910cc5
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-14 22:07:54 -0400

    refactor: change naming Guacamaya to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Banner from "./Guacamaya/Banner"
-import Form from "./Guacamaya/Form"
-import Navbar from "./Guacamaya/Navbar"
-import FAQS from "./Guacamaya/FAQS"
-import Table from "./Guacamaya/FlightPlans"
-import Footer from "./Guacamaya/Footer"
-import OurPlaces from "./Guacamaya/OurPlaces/OurPlaces"
+import Banner from "./pages/Banner"
+import Form from "./pages/Form"
+import Navbar from "./pages/Navbar"
+import FAQS from "./pages/FAQS"
+import Table from "./pages/FlightPlans"
+import Footer from "./pages/Footer"
+import OurPlaces from "./pages/OurPlaces/OurPlaces"
 
 
 function App() {
