358c05d44f1909f89fd6b67d7a6f22d410c610a7
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-14 22:34:52 -0400

    chore: change folder

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,17 @@
+import Banner from "./Banner"
+import Form from "./Form"
+import Navbar from "./Navbar"
+import FAQS from "./FAQS"
+import Table from "./FlightPlans"
+import Footer from "./Footer"
+import OurPlaces from "./OurPlaces"
+
+export {
+    Banner,
+    Form,
+    Navbar,
+    FAQS,
+    Table,
+    Footer,
+    OurPlaces
+}
