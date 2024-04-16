35272912a700b1ea79187ba2884396492801728d
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-04-16 01:03:13 -0400

    refactor:add index in assets to import the images

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,13 @@
+// assets/index.js
+
+export { default as Logo } from './logo_guacamaya.svg';
+export { default as Icon } from './icon_guacamaya.svg';
+export { default as LosRoques } from './Los_roques.svg';
+export { default as Margarita } from './Margarita.svg';
+export { default as Medanos } from './Medanos_de_coro.svg';
+export { default as Mochima } from './Mochima.svg';
+export { default as Merida } from './Merida.svg';
+export { default as Morrocoy } from './Morrocoy.svg';
+export { default as Roraima } from './Roraima.svg';
+export { default as Puerto } from './Puerto_la_cruz.svg';
+export { default as Avila } from './Avila.jpg';
