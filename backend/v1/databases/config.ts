e7d65a39a928886326369f5871cbb4872922f413
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-16 17:46:33 -0400

    feat: add entity & AppDataSource

--- backend/v1/databases/config.ts
+++ backend/v1/databases/config.ts
@@ -1,8 +1,11 @@
 import { config } from './../configuration/config'
 
+import { DataSource } from 'typeorm'
+
 const { type, host, port, username, password, database } = config.database
 
 export const AppDataSource = new DataSource({
+  //@ts-ignore
   type,
   host,
   port,
@@ -15,3 +18,12 @@ export const AppDataSource = new DataSource({
   subscribers: [],
   migrations: [],
 })
+
+// to initialize the initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch((error) => console.log(error))
---
+++ backend/v1/databases/entity/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt!: Date
+}
