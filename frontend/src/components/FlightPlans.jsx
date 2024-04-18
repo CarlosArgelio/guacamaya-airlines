8931fb337dca0fcc3f12ba1517db82f2a057ae97
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-04-18 16:55:10 -0400

    refactor:improved the code in the flight plans section

--- frontend/src/components/FlightPlans.jsx
+++ frontend/src/components/FlightPlans.jsx
@@ -6,53 +6,98 @@ function Table() {
         <table className="table-auto w-full border-collapse border border-gray-400">
           <thead>
             <tr className="bg-gray-200">
-              <th className="px-4 py-2 text-left">Número de Vuelo</th>
-              <th className="px-4 py-2 text-left">Origen</th>
-              <th className="px-4 py-2 text-left">Destino</th>
               <th className="px-4 py-2 text-left">Hora de Salida</th>
-              <th className="px-4 py-2 text-left">Hora de Llegada</th>
+              <th className="px-4 py-2 text-left">Destino</th>
+              <th className="px-4 py-2 text-left">Código de vuelo</th>
+              <th className="px-4 py-2 text-left">Puerta</th>
+              <th className="px-4 py-2 text-left">Status</th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">FL123</td>
-              <td className="border px-4 py-2">Ciudad A</td>
-              <td className="border px-4 py-2">Ciudad B</td>
-              <td className="border px-4 py-2">08:00</td>
-              <td className="border px-4 py-2">10:00</td>
-            </tr>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">FL123</td>
-              <td className="border px-4 py-2">Ciudad A</td>
-              <td className="border px-4 py-2">Ciudad B</td>
-              <td className="border px-4 py-2">08:00</td>
-              <td className="border px-4 py-2">10:00</td>
-            </tr>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">FL123</td>
-              <td className="border px-4 py-2">Ciudad A</td>
-              <td className="border px-4 py-2">Ciudad B</td>
-              <td className="border px-4 py-2">08:00</td>
-              <td className="border px-4 py-2">10:00</td>
-            </tr>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">FL123</td>
-              <td className="border px-4 py-2">Ciudad A</td>
-              <td className="border px-4 py-2">Ciudad B</td>
-              <td className="border px-4 py-2">08:00</td>
-              <td className="border px-4 py-2">10:00</td>
-            </tr>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">FL123</td>
-              <td className="border px-4 py-2">Ciudad A</td>
-              <td className="border px-4 py-2">Ciudad B</td>
-              <td className="border px-4 py-2">08:00</td>
-              <td className="border px-4 py-2">10:00</td>
-            </tr>
+            {
+              FlightPlans.map(flightPlan => (
+
+              <tr key={flightPlan.id} className="bg-white">
+                <td className="border px-4 py-2">{flightPlan.leave}</td>
+                <td className="border px-4 py-2">{flightPlan.destination}</td>
+                <td className="border px-4 py-2">{flightPlan.code}</td>
+                <td className="border px-4 py-2">{flightPlan.door}</td>
+                <td className="border px-4 py-2">{flightPlan.status}</td>
+              </tr>
+              ))
+            }
+           
           </tbody>
         </table>
       </div>
     );
   }
+
+  const FlightPlans = [
+    {
+      id: 1,
+      leave: '12:39',
+      destination: 'Roraima',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Confirmado',
+    },
+    {
+      id: 2,
+      leave: '12:39',
+      destination: 'Mochima',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Esperando',
+    },
+    {
+      id: 3,
+      leave: '12:39',
+      destination: 'Merida',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Saliendo',
+    },
+    {
+      id: 4,
+      leave: '12:39',
+      destination: 'Morrocoy',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Cancelado',
+    },
+    {
+      id: 5,
+      leave: '12:39',
+      destination: 'Margarita',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Confirmado',
+    },
+    {
+      id: 6,
+      leave: '12:39',
+      destination: 'Puerto la cruz',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Esperando',
+    },
+    {
+      id: 7,
+      leave: '12:39',
+      destination: 'Medanos de Coro',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Saliendo',
+    },
+    {
+      id: 8,
+      leave: '12:39',
+      destination: 'Los roques',
+      code: '2913 - CJT',
+      door: '01',
+      status: 'Cancelado',
+    },
+  ]
 
   export default Table;
