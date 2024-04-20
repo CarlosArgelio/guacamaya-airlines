6af738911b3d52f0797671d41d8cb8f6866d89ad
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-20 18:44:31 -0400

    feat: integrate db to airports

--- backend/v1/databases/index.ts
+++ backend/v1/databases/index.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from './config'
 
 import { Reservations } from './entity/reservation.entity'
+import { Airports } from './entity/airport.entity'
 
-export { AppDataSource, Reservations }
+export { AppDataSource, Reservations, Airports }
