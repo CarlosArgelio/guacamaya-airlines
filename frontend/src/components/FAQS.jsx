4b6dc6b74d4aed03d41ff8e3f8c11f1bd7fe190b
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-04-20 19:18:33 -0400

    style: the style was changed

--- frontend/src/components/FAQS.jsx
+++ frontend/src/components/FAQS.jsx
@@ -1,9 +1,15 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 
 
 function Faqs() {
   return (
     <div className="max-w-6xl mx-auto py-8">
+       <div className="flex">
+
       <h2 className="text-3xl font-semibold mb-4">Preguntas Frecuentes</h2>
+      <QuestionMarkCircleIcon className="w-10 h-10 "></QuestionMarkCircleIcon>
+       </div>
+
       <div className="divide-y divide-gray-300">
         {
           FAQS.map(faq => (
--- frontend/src/components/OurPlaces.jsx
+++ frontend/src/components/OurPlaces.jsx
@@ -1,3 +1,5 @@
+import { GlobeAmericasIcon } from "@heroicons/react/24/outline";
+
 import { Roraima } from "../assets";
 import { Mochima } from "../assets";
 import { Merida } from "../assets";
@@ -11,15 +13,13 @@ import { Puerto } from "../assets";
 // eslint-disable-next-line react/prop-types
 function Card({ imageSrc, title, description }) {
   return (
-    <div className="w-full md:w-1/4 px-6 mb-10">
-      <div className="relative">
-        <img src={imageSrc} alt={title} className="w-50 h-80 rounded-xl" />
-        <div className="absolute inset-0 bg-black rounded-xl opacity-0 hover:opacity-75 transition duration-300">
-          <div className="absolute inset-0 flex justify-center items-center">
-            <div className="text-center">
-              <h3 className="text-white text-lg font-semibold mb-2">{title}</h3>
-              <p className="text-gray-200 px-5 text-justify">{description}</p>
-            </div>
+    <div className="w-full md:w-1/3 lg:w-1/4 px-4 mb-10 flex justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="relative shadow-2xl  rounded-3xl">
+        <img src={imageSrc} alt={title} className="w-full h-auto rounded-3xl" />
+        <div className="absolute inset-0 bg-black rounded-3xl opacity-0 hover:opacity-75 transition duration-300 flex justify-center items-center">
+          <div className="text-center">
+            <h3 className="text-white text-lg font-semibold mb-2">{title}</h3>
+            <p className="text-gray-200 px-5 text-justify">{description}</p>
           </div>
         </div>
       </div>
@@ -30,8 +30,12 @@ function Card({ imageSrc, title, description }) {
 function OurPlaces() {
   return (
     <div className="container max-w-6xl mx-auto py-8">
-        <h1 className='text-3xl font-semibold mb-8'>Nuestros destinos</h1>
-      <div className="flex flex-wrap -mx-4">
+        <div className="flex">
+
+      <h1 className="text-3xl font-semibold mb-8">Nuestros destinos</h1>
+      <GlobeAmericasIcon className="w-10 h-10"></GlobeAmericasIcon>
+        </div>
+      <div className="flex flex-wrap -mx-4 justify-center">
         {cards.map((card, index) => (
           <Card
             key={index}
