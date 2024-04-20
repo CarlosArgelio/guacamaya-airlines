27c688451799e392d4bee68ef255407318cd2bf6
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-20 18:28:27 -0400

    feat: remove moudle destination

--- backend/v1/networks/index.ts
+++ backend/v1/networks/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 
 import { airpot } from './airport'
-import { destination } from './destination'
 import { reserver } from './reservation'
 
 export const networks = () => {
   const router = Router()
   router.use('/airports', airpot)
-  router.use('/destinations', destination)
   router.use('/reservations', reserver)
 
   return router
