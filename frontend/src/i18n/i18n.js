6e340510d7c6cda748116b92cb81dba10e935c8e
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-21 11:39:10 -0400

    feat: config init i18n

--- frontend/src/i18n/i18n.js
+++ frontend/src/i18n/i18n.js
@@ -1,6 +1,13 @@
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import EN from "./en.json";
+import ES from "./es.json";
+
+
 i18n.use(initReactI18next).init({
-    resources: {}, // Where we're gonna put translations' files
+    resources: {
+        en: { ...EN },
+        es: { ...ES }
+    }, // Where we're gonna put translations' files
     lng: "en",     // Set the initial language of the App
 });
