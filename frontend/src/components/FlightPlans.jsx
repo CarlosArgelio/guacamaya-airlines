698083e6c8eead1a4d7c833aa1187feef8f3dec9
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-21 15:57:33 -0400

    feat: get data Fligth plans

--- frontend/src/components/FlightPlans.jsx
+++ frontend/src/components/FlightPlans.jsx
@@ -1,113 +1,54 @@
 import { CalendarIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { getAllFligthPlans } from "./../services/api";
 
 function Table() {
-      // eslint-disable-next-line no-unused-vars
-      const [t, i18n] = useTranslation("global");
+  const [fligth, setFligth] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [t, i18n] = useTranslation("global");
 
-    return (
-      <div className="container max-w-6xl mx-auto py-8 ">
-        <div className="flex">
+  useEffect(() => {
+    getAllFligthPlans(setFligth);
+  }, []);
 
-          <h1 className="text-3xl font-semibold mb-8">{t('table.fligthPlans')}</h1>
+  if (!fligth) return null;
 
-          <CalendarIcon className="w-10 h-10 "></CalendarIcon>
-        </div>
-        <table className="table-auto w-full border-collapse border border-gray-300">
-          <thead>
-            <tr className="bg-gray-900 text-yellow-300">
-              <th className="px-4 py-2 text-left">{t('table.hours')}</th>
-              <th className="px-4 py-2 text-left">{t('table.destination')}</th>
-              <th className="px-4 py-2 text-left">{t('table.code')}</th>
-              <th className="px-4 py-2 text-left">{t('table.door')}</th>
-              <th className="px-4 py-2 text-left">{t('table.status')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              FlightPlans.map(flightPlan => (
+  return (
+    <div className="container max-w-6xl mx-auto py-8 ">
+      <div className="flex">
+        <h1 className="text-3xl font-semibold mb-8">
+          {t("table.fligthPlans")}
+        </h1>
 
-              <tr key={flightPlan.id} className="bg-gray-900 text-yellow-300">
-                <td className="border px-4 py-2">{flightPlan.leave}</td>
-                <td className="border px-4 py-2">{flightPlan.destination}</td>
-                <td className="border px-4 py-2">{flightPlan.code}</td>
-                <td className="border px-4 py-2">{flightPlan.door}</td>
-                <td className="border px-4 py-2">{flightPlan.status}</td>
-              </tr>
-              ))
-            }
-           
-          </tbody>
-        </table>
+        <CalendarIcon className="w-10 h-10 "></CalendarIcon>
       </div>
-    );
-  }
+      <table className="table-auto w-full border-collapse border border-gray-300">
+        <thead>
+          <tr className="bg-gray-900 text-yellow-300">
+            <th className="px-4 py-2 text-left">{t("table.hours")}</th>
+            <th className="px-4 py-2 text-left">{t("table.destination")}</th>
+            <th className="px-4 py-2 text-left">{t("table.code")}</th>
+            <th className="px-4 py-2 text-left">{t("table.door")}</th>
+            <th className="px-4 py-2 text-left">{t("table.status")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fligth.map((flightPlan) => (
+            <tr key={flightPlan.id} className="bg-gray-900 text-yellow-300">
+              <td className="border px-4 py-2">{flightPlan.departureTime}</td>
+              <td className="border px-4 py-2">{flightPlan.destination}</td>
+              <td className="border px-4 py-2">{flightPlan.code}</td>
+              <td className="border px-4 py-2">{flightPlan.door}</td>
+              <td className="border px-4 py-2">
+                {flightPlan.status ? "CONFIRMADO" : "EN ESPERA"}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-  const FlightPlans = [
-    {
-      id: 1,
-      leave: '12:39',
-      destination: 'Roraima',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Confirmado',
-    },
-    {
-      id: 2,
-      leave: '12:39',
-      destination: 'Mochima',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Esperando',
-    },
-    {
-      id: 3,
-      leave: '12:39',
-      destination: 'Merida',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Saliendo',
-    },
-    {
-      id: 4,
-      leave: '12:39',
-      destination: 'Morrocoy',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Cancelado',
-    },
-    {
-      id: 5,
-      leave: '12:39',
-      destination: 'Margarita',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Confirmado',
-    },
-    {
-      id: 6,
-      leave: '12:39',
-      destination: 'Puerto la cruz',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Esperando',
-    },
-    {
-      id: 7,
-      leave: '12:39',
-      destination: 'Medanos de Coro',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Saliendo',
-    },
-    {
-      id: 8,
-      leave: '12:39',
-      destination: 'Los roques',
-      code: '2913 - CJT',
-      door: '01',
-      status: 'Cancelado',
-    },
-  ]
-
-  export default Table;
+export default Table;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,16 @@
-import axios from "axios"
-import { API } from "../configuration/config"
+import axios from "axios";
+import { API } from "../configuration/config";
 
 export const getAllAirports = (setAirPort) => {
-  return axios.get(`${API}/api/v1/airports/`)
-  .then((response) => {
-    setAirPort(response.data.data)
+  return axios.get(`${API}/api/v1/airports/`).then((response) => {
+    setAirPort(response.data.data);
   });
-}
+};
 
+export const getAllFligthPlans = (FligthPlans) => {
+  return axios
+    .get(`${API}/api/v1/reservations/flight-plans/`)
+    .then((response) => {
+      FligthPlans(response.data.data);
+    });
+};
